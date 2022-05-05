94be28b9838951debc53756fa3729bd2e055a297
Roald Välja <roaldvlja@gmail.com>
2022-05-05 13:18:06 +0300

    Roald login branch test

--- app/src/main/java/com/example/androidreviewapp/MainActivity.java
+++ app/src/main/java/com/example/androidreviewapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Comment by Roald
+        // Comment by Roald login
     }
 }
