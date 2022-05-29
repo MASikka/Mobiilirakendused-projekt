932a4b1b450c47db3515fb1190d61734b7ab4658
Roald Välja <roaldvlja@gmail.com>
2022-05-29 13:53:59 +0300

    ADD genres recyclerview

--- app/src/main/java/com/example/androidreviewapp/adapter/GenreAdapter.java
+++ app/src/main/java/com/example/androidreviewapp/adapter/GenreAdapter.java
@@ -1,4 +1,55 @@
 package com.example.androidreviewapp.adapter;
 
-public class GenreAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidreviewapp.R;
+import com.example.androidreviewapp.model.Genre;
+
+import java.util.ArrayList;
+
+public class GenreAdapter extends RecyclerView.Adapter<GenreAdapter.GenreViewHolder>{
+
+    ArrayList<String> genreList;
+
+    public GenreAdapter() {
+        this.genreList = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public GenreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_game_genre, parent, false);
+        return new GenreViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GenreViewHolder holder, int position) {
+        String genre = genreList.get(position);
+        holder.txtGenre.setText(genre);
+    }
+
+    @Override
+    public int getItemCount() {
+        return genreList.size();
+    }
+
+    public void setGenreList(final ArrayList<String> genreList){
+        this.genreList = genreList;
+        notifyDataSetChanged();
+    }
+
+    class GenreViewHolder extends RecyclerView.ViewHolder{
+        private final TextView txtGenre;
+
+        public GenreViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtGenre = itemView.findViewById(R.id.txtGenre);
+        }
+    }
 }
--- app/src/main/java/com/example/androidreviewapp/model/Genre.java
+++ app/src/main/java/com/example/androidreviewapp/model/Genre.java
@@ -2,5 +2,17 @@ package com.example.androidreviewapp.model;
 
 public class Genre {
 
+    private String genreName;
 
+    public Genre(String genreName) {
+        this.genreName = genreName;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
 }
