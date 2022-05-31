39b6580989bcdde6d2161a3dd59c5c740927f50d
Marek <mareksikka@gmail.com>
2022-05-31 18:31:29 +0300

    Fix settingsfragment

--- app/src/main/java/com/example/androidreviewapp/fragment/SettingsFragment.java
+++ app/src/main/java/com/example/androidreviewapp/fragment/SettingsFragment.java
@@ -1,10 +1,12 @@
-package com.example.androidreviewapp;
+package com.example.androidreviewapp.fragment;
 
 import android.os.Bundle;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.androidreviewapp.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
