55bd214f5d688b266655e3d66e12cd9e8e51813d
T6nuk <t6nu.koll@gmail.com>
2022-05-31 17:58:55 +0300

    initialprice finalprice strings.xml changes

--- app/src/main/java/com/example/androidreviewapp/MainActivity.java
+++ app/src/main/java/com/example/androidreviewapp/MainActivity.java
@@ -1,18 +1,30 @@
 package com.example.androidreviewapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private TextView themeTV;
+    WebViewFeature webViewFeature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //
+        //if(WebViewFeature.isFeatureSupported(webViewFeature.FORCE_DARK)) {
+        //            WebSettingsCompat.getForceDark()
+        //        }
 
     }
 
