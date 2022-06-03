bf4bb2e458b3e042ff229f8fcbe01e84637f69c2
Marek <mareksikka@gmail.com>
2022-06-03 22:25:19 +0300

    FIX register navigation

--- app/src/main/java/com/example/androidreviewapp/fragment/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/androidreviewapp/fragment/ForgotPasswordFragment.java
@@ -3,6 +3,7 @@ package com.example.androidreviewapp.fragment;
 
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 import com.example.androidreviewapp.R;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.androidreviewapp.viewmodel.LoginViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -46,17 +48,21 @@ public class ForgotPasswordFragment extends Fragment {
         userButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(TextUtils.isEmpty(recovery_email.getText().toString())){} else{
                 firebaseAuth.sendPasswordResetEmail(recovery_email.getText().toString())
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful()) {
+
+                                    Navigation.findNavController(getView())
+                                            .navigate(R.id.action_forgotPasswordFragment_to_loginFragment);
                                     Toast.makeText(getActivity(), getString(R.string.password_sent), Toast.LENGTH_SHORT).show();
                                 } else {
                                     Toast.makeText(getActivity(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             }
-                        });
+                        });}
             }
         });
 
